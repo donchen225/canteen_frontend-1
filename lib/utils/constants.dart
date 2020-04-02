2236ebd6ae26ba3caaa6101a93c6baf37a5e19d7
brianbhsu <brianhsu93@gmail.com>
2020-04-02 14:25:02 -0400

    update constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,9 @@
 // Login/Sign-up constants
-const int FORM_REFRESH_MILLISECONDS = 300;
+const int formRefreshMilliseconds = 300;
 
 // Firebase Storage constants
-const String firebase_storage_url = 'gs://get-canteen.appspot.com';
+const String firebaseStorageUrl = 'gs://get-canteen.appspot.com';
 
-const int FIREBASE_STORAGE_MAX_UPLOAD_SIZE = 10000000;
+const int firebaseStorageMaxUploadSize = 10000000;
 
 // Theme constants
