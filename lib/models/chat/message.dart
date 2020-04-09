f54875e2e5179a019fa7a39f90a9ea1c4645bd74
brianbhsu <brianhsu93@gmail.com>
2020-04-09 12:52:16 -0400

    added MessageType

--- lib/models/chat/message.dart
+++ lib/models/chat/message.dart
@@ -1,16 +1,18 @@
-import 'package:canteen_frontend/models/chat/chat_entity.dart';
 import 'package:canteen_frontend/models/chat/message_entity.dart';
+import 'package:canteen_frontend/models/chat/message_type.dart';
 import 'package:meta/meta.dart';
 
 @immutable
 class Message {
   final String id;
   final String senderId;
+  final MessageType type;
   final DateTime timestamp;
 
   Message({
     @required this.id,
     @required this.senderId,
+    @required this.type,
     @required this.timestamp,
   });
 
@@ -18,6 +20,7 @@ class Message {
     return Message(
       id: entity.id,
       senderId: entity.senderId,
+      type: MessageType.values[entity.type],
       timestamp: entity.timestamp,
     );
   }
@@ -26,6 +29,7 @@ class Message {
     return MessageEntity(
       id: id,
       senderId: senderId,
+      type: type.index,
       timestamp: timestamp,
     );
   }
--- lib/models/chat/message_entity.dart
+++ lib/models/chat/message_entity.dart
@@ -4,22 +4,27 @@ import 'package:meta/meta.dart';
 class MessageEntity extends Equatable {
   final String id;
   final String senderId;
+  final int type;
   final DateTime timestamp;
 
   const MessageEntity(
-      {@required this.id, @required this.senderId, @required this.timestamp});
+      {@required this.id,
+      @required this.senderId,
+      @required this.type,
+      @required this.timestamp});
 
   @override
-  List<Object> get props => [id, senderId, timestamp];
+  List<Object> get props => [id, senderId, type, timestamp];
 
   @override
   String toString() {
-    return 'MessageEntity { id: $id, senderId: $senderId, timestamp: $timestamp }';
+    return 'MessageEntity { id: $id, senderId: $senderId, type: $type, timestamp: $timestamp }';
   }
 
   Map<String, Object> toDocument() {
     return {
       'sender_id': senderId,
+      'type': type,
       'timestamp': timestamp,
     };
   }
