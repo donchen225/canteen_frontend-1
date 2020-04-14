61fd03bdb26ba3d40ffe9cdbec54c6aacdee491a
brianbhsu <brianhsu93@gmail.com>
2020-04-14 19:32:28 -0400

    added response UI to ProfileList

--- lib/screens/profile/profile_list.dart
+++ lib/screens/profile/profile_list.dart
@@ -3,6 +3,7 @@ import 'package:canteen_frontend/screens/profile/profile_picture.dart';
 import 'package:canteen_frontend/screens/profile/profile_section_title.dart';
 import 'package:canteen_frontend/screens/profile/profile_text_card.dart';
 import 'package:canteen_frontend/screens/profile/skill_list.dart';
+import 'package:canteen_frontend/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
 class ProfileList extends StatelessWidget {
@@ -35,12 +36,13 @@ class ProfileList extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                padding: EdgeInsets.only(
+                    bottom: SizeConfig.instance.blockSizeVertical * 3),
                 child: ProfilePicture(
                   photoUrl: user.photoUrl,
                   localPicture: AssetImage('assets/blank-profile-picture.jpeg'),
                   editable: false,
-                  size: 160,
+                  size: SizeConfig.instance.blockSizeHorizontal * 50,
                 ),
               ),
             ],
