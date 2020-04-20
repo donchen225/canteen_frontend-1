8f2f8cc7ca4cdbc38adc792e857d61a48c8f09c5
brianbhsu <brianhsu93@gmail.com>
2020-04-20 15:10:25 -0400

    updated custom onboarding package

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -9,16 +9,32 @@ class OnboardingScreen extends StatelessWidget {
     pageColor: const Color(0xFF607D8B),
     // iconImageAssetPath: 'assets/taxi-driver.png',
     iconColor: null,
-    bubbleBackgroundColor: Colors.transparent,
-    body: Text(
-      'Easy  cab  booking  at  your  doorstep  with  cashless  payment  system',
+    bubbleBackgroundColor: Colors.red,
+    body: Visibility(
+      visible: false,
+      child: Container(
+        color: Colors.red,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Text(
+              'Easy  cab  booking  at  your  doorstep  with  cashless  payment  system',
+            ),
+          ],
+        ),
+      ),
     ),
-    title: Text('Cabs'),
-    mainImage: Image.asset(
-      'assets/icon.jpeg',
-      height: 285.0,
-      width: 285.0,
-      alignment: Alignment.center,
+    title: Visibility(visible: false, child: Container()),
+    mainImage: Container(
+      color: Colors.red,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Text(
+            'Easy  cab  booking  at  your  doorstep  with  cashless  payment  system',
+          ),
+        ],
+      ),
     ),
     titleTextStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
     bodyTextStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
@@ -27,11 +43,12 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IntroViewsFlutter(
-      [page],
+      [page, page, page, page, page, page],
       onTapDoneButton: () {
         BlocProvider.of<HomeBloc>(context).add(PageTapped(index: 0));
       },
-      showSkipButton: true,
+      showNextButton: true,
+      showSkipButton: false,
       pageButtonTextStyles: TextStyle(
         color: Colors.white,
         fontSize: 18.0,
