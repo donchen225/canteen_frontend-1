a7f7ab5b1e065f609a5e29b53ea02241e8f0c8ca
brianbhsu <brianhsu93@gmail.com>
2020-04-20 21:09:07 -0400

    updated splash screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,10 +1,54 @@
+import 'package:canteen_frontend/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 2000),
+    );
+    animationController.forward();
+    animationController.addListener(() {
+      setState(() {
+        if (animationController.status == AnimationStatus.completed) {
+          animationController.repeat();
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Splash Screen')),
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.white,
+      child: RotationTransition(
+        turns: Tween(begin: 0.0, end: 1.0).animate(animationController),
+        child: Container(
+          height: SizeConfig.instance.blockSizeHorizontal * 30,
+          width: SizeConfig.instance.blockSizeHorizontal * 30,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/loading-icon.png'),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
