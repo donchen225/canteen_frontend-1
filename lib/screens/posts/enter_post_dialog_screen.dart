351193e576e9fc4362c9cc55537101b95f27e18e
brianbhsu <brianhsu93@gmail.com>
2020-05-07 13:07:33 -0400

    add post screen WIP

--- lib/screens/posts/enter_post_dialog_screen.dart
+++ lib/screens/posts/enter_post_dialog_screen.dart
@@ -23,84 +23,102 @@ class _EnterPostDialogScreenState extends State<EnterPostDialogScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: SizeConfig.instance.blockSizeVertical * 90,
-      decoration: BoxDecoration(
-        color: const Color(0xFFFEFFFF),
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(15),
-          topRight: Radius.circular(15),
-        ),
-      ),
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-              top: SizeConfig.instance.blockSizeVertical * 2,
-              bottom: SizeConfig.instance.blockSizeVertical * 2,
-            ),
-            decoration: BoxDecoration(
-              color: const Color(0xFFFEFFFF),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'New Question',
-                  style:
-                      TextStyle(fontSize: 18, color: const Color(0xFF939598)),
-                ),
-              ],
-            ),
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Container(
+        height: SizeConfig.instance.blockSizeVertical * 90,
+        decoration: BoxDecoration(
+          color: const Color(0xFFFEFFFF),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
           ),
-          Container(
-            padding: EdgeInsets.only(
-              top: SizeConfig.instance.blockSizeVertical * 3,
-              left: SizeConfig.instance.blockSizeHorizontal * 3,
-              right: SizeConfig.instance.blockSizeHorizontal * 3,
-            ),
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  width: 1,
-                  color: const Color(0xFFDEE0D1),
-                ),
-                bottom: BorderSide(
-                  width: 1,
-                  color: const Color(0xFFDEE0D1),
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(
+                top: SizeConfig.instance.blockSizeVertical * 2,
+                bottom: SizeConfig.instance.blockSizeVertical * 2,
+              ),
+              decoration: BoxDecoration(
+                color: const Color(0xFFFEFFFF),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
                 ),
               ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'New Question',
+                    style:
+                        TextStyle(fontSize: 18, color: const Color(0xFF939598)),
+                  ),
+                ],
+              ),
             ),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[Text('Brian Hsu asked')],
+            Container(
+              padding: EdgeInsets.only(
+                top: SizeConfig.instance.blockSizeVertical * 3,
+                left: SizeConfig.instance.blockSizeHorizontal * 3,
+                right: SizeConfig.instance.blockSizeHorizontal * 3,
+              ),
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    width: 1,
+                    color: const Color(0xFFDEE0D1),
+                  ),
+                  bottom: BorderSide(
+                    width: 1,
+                    color: const Color(0xFFDEE0D1),
+                  ),
                 ),
-                TextField(
-                  controller: _textController,
-                  maxLines: null,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    RaisedButton(
-                      color: Palette.orangeColor,
-                      child: Text(
-                        'Add',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+              ),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text('Brian Hsu asked'),
+                    ],
+                  ),
+                  TextField(
+                    controller: _textController,
+                    autofocus: true,
+                    maxLines: null,
+                    decoration: InputDecoration(hintText: 'Add your question'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      RaisedButton(
+                        color: Palette.orangeColor,
+                        child: Text(
+                          'Add',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      onPressed: _textController.text.isNotEmpty ? () {} : null,
-                    )
-                  ],
-                )
-              ],
+                        onPressed:
+                            _textController.text.isNotEmpty ? () {} : null,
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
