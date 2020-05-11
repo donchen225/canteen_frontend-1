181d55e69d1e363fd7bed67cc6087b83ce8f509b
brianbhsu <brianhsu93@gmail.com>
2020-05-11 17:17:09 -0400

    new layout and styling

--- lib/screens/search/discover_screen.dart
+++ lib/screens/search/discover_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:canteen_frontend/models/user/user.dart';
+import 'package:canteen_frontend/screens/search/profile_card.dart';
 import 'package:canteen_frontend/screens/search/search_bloc/bloc.dart';
 import 'package:canteen_frontend/utils/size_config.dart';
 import 'package:flutter/cupertino.dart';
@@ -15,105 +16,71 @@ class DiscoverScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: <Widget>[
-        SliverPadding(
-          padding: EdgeInsets.only(top: SizeConfig.instance.safeBlockVertical),
-          sliver: SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: SizeConfig.instance.safeBlockVertical * 3,
+              bottom: SizeConfig.instance.safeBlockVertical,
+              left: SizeConfig.instance.safeBlockHorizontal * 6,
+              right: SizeConfig.instance.safeBlockHorizontal * 6,
+            ),
+            child: Text('Recommended For You',
+                style: Theme.of(context).textTheme.headline6.apply(
+                      fontFamily: '.SF UI Text',
+                    )),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            height: SizeConfig.instance.safeBlockVertical * 30,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 100.0,
+                  child: Card(
+                    child: Text('data'),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: SizeConfig.instance.safeBlockVertical * 3,
+              bottom: SizeConfig.instance.safeBlockVertical,
+              left: SizeConfig.instance.safeBlockHorizontal * 6,
+              right: SizeConfig.instance.safeBlockHorizontal * 6,
+            ),
+            child: Text('Most Popular',
+                style: Theme.of(context).textTheme.headline6.apply(
+                      fontFamily: '.SF UI Text',
+                    )),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            height: SizeConfig.instance.safeBlockVertical * 40,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: userList.length,
+              itemBuilder: (context, index) {
                 final user = userList[index];
                 return Padding(
                   padding: EdgeInsets.only(
                       left: SizeConfig.instance.safeBlockHorizontal * 3,
                       right: SizeConfig.instance.safeBlockHorizontal * 3,
                       bottom: SizeConfig.instance.safeBlockVertical),
-                  child: GestureDetector(
-                    onTap: () {
-                      BlocProvider.of<SearchBloc>(context)
-                          .add(SearchInspectUser(user));
-                    },
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      child: Container(
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              height:
-                                  SizeConfig.instance.blockSizeVertical * 33,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(15),
-                                    topRight: Radius.circular(15)),
-                                image: DecorationImage(
-                                  image: (user.photoUrl != null &&
-                                          user.photoUrl.isNotEmpty)
-                                      ? CachedNetworkImageProvider(
-                                          user.photoUrl)
-                                      : AssetImage(
-                                          'assets/blank-profile-picture.jpeg'),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.only(
-                                top: SizeConfig.instance.safeBlockVertical * 2,
-                                bottom:
-                                    SizeConfig.instance.safeBlockVertical * 2,
-                                left:
-                                    SizeConfig.instance.safeBlockHorizontal * 6,
-                                right:
-                                    SizeConfig.instance.safeBlockHorizontal * 6,
-                              ),
-                              child: Column(
-                                children: <Widget>[
-                                  Visibility(
-                                    visible: user.title?.isNotEmpty ?? false,
-                                    child: Padding(
-                                      padding: EdgeInsets.only(
-                                          bottom: SizeConfig
-                                              .instance.safeBlockVertical),
-                                      child: Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: Text(user.title ?? '',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline6),
-                                      ),
-                                    ),
-                                  ),
-                                  Visibility(
-                                    visible: user.teachSkill.length != 0,
-                                    child: ListView.builder(
-                                      physics: NeverScrollableScrollPhysics(),
-                                      shrinkWrap: true,
-                                      itemCount: user.teachSkill.length,
-                                      itemBuilder: (context, index) {
-                                        final skill = user.teachSkill[index];
-                                        return Padding(
-                                          padding:
-                                              EdgeInsets.symmetric(vertical: 1),
-                                          child: Text(
-                                            skill.name,
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w800),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                  child: ProfileCard(
+                      user: user,
+                      height: SizeConfig.instance.safeBlockVertical * 40,
+                      onTap: () => BlocProvider.of<SearchBloc>(context)
+                          .add(SearchInspectUser(user))),
                 );
               },
-              childCount: userList.length,
             ),
           ),
         ),
---
+++ lib/screens/search/profile_card.dart
@@ -0,0 +1,99 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:canteen_frontend/models/user/user.dart';
+import 'package:canteen_frontend/utils/size_config.dart';
+import 'package:flutter/material.dart';
+
+class ProfileCard extends StatelessWidget {
+  final User user;
+  final Function onTap;
+  final double height;
+  final double width;
+
+  const ProfileCard({
+    Key key,
+    this.onTap,
+    this.height = 400,
+    this.width = 300,
+    @required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap();
+        }
+      },
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+            // color: Colors.red,
+            ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: height * 0.5,
+              width: width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
+                image: DecorationImage(
+                  image: (user.photoUrl != null && user.photoUrl.isNotEmpty)
+                      ? CachedNetworkImageProvider(user.photoUrl)
+                      : AssetImage('assets/blank-profile-picture.jpeg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              width: width,
+              padding: EdgeInsets.only(
+                top: SizeConfig.instance.safeBlockVertical * 2,
+                bottom: SizeConfig.instance.safeBlockVertical * 2,
+                left: SizeConfig.instance.safeBlockHorizontal * 6,
+                right: SizeConfig.instance.safeBlockHorizontal * 6,
+              ),
+              child: Column(
+                children: <Widget>[
+                  Visibility(
+                    visible: user.title?.isNotEmpty ?? false,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          bottom: SizeConfig.instance.safeBlockVertical),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(user.title ?? '',
+                            style: Theme.of(context).textTheme.headline6),
+                      ),
+                    ),
+                  ),
+                  Visibility(
+                    visible: user.teachSkill.length != 0,
+                    child: ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: user.teachSkill.length,
+                      itemBuilder: (context, index) {
+                        final skill = user.teachSkill[index];
+                        return Padding(
+                          padding: EdgeInsets.symmetric(vertical: 1),
+                          child: Text(
+                            skill.name,
+                            style: TextStyle(fontWeight: FontWeight.w800),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
