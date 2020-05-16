f1a8a3049234a70f786555e5ace6a62396669a64
brianbhsu <brianhsu93@gmail.com>
2020-05-16 14:58:57 -0400

    finall have something that kind of works

--- lib/screens/search/view_user_profile_screen.dart
+++ lib/screens/search/view_user_profile_screen.dart
@@ -69,227 +69,130 @@ class _ViewUserProfileScreenState extends State<ViewUserProfileScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Palette.containerColor,
+      appBar: AppBar(
+        backgroundColor: Palette.containerColor,
+        elevation: 0,
+        leading: BackButton(
+          onPressed: () {
+            if (widget.onTapBack != null) {
+              widget.onTapBack();
+            }
+          },
+        ),
+      ),
       body: NestedScrollView(
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          // These are the slivers that show up in the "outer" scroll view.
           return <Widget>[
-            SliverOverlapAbsorber(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-              sliver: SliverAppBar(
-                leading: Container(
-                  color: Colors.red,
-                  child: BackButton(
-                    onPressed: () {
-                      if (widget.onTapBack != null) {
-                        widget.onTapBack();
-                      }
-                    },
-                  ),
-                ),
-                title: Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: Container(
-                            height: kToolbarHeight,
-                            color: Colors.red,
-                            child: Text('Test'))),
-                  ],
-                ),
-                elevation: 0,
-                primary: true,
-                backgroundColor: Palette.appBarBackgroundColor,
-                pinned: true,
-                // forceElevated: innerBoxIsScrolled,
-                expandedHeight: SizeConfig.instance.safeBlockVertical * 45,
-                flexibleSpace: FlexibleSpaceBar(
-                  collapseMode: CollapseMode.pin,
-                  background: SafeArea(
-                    top: true,
-                    child: Container(
-                      height: SizeConfig.instance.safeBlockVertical * 35,
-                      padding: EdgeInsets.only(
-                        top: kToolbarHeight,
-                        bottom: kToolbarHeight,
-                        left: SizeConfig.instance.safeBlockHorizontal * 6,
-                        right: SizeConfig.instance.safeBlockHorizontal * 6,
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            height:
-                                SizeConfig.instance.safeBlockHorizontal * 30,
-                            padding: EdgeInsets.only(
-                              bottom: SizeConfig.instance.safeBlockVertical,
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                ProfilePicture(
-                                  photoUrl: widget.user.photoUrl,
-                                  shape: BoxShape.circle,
-                                  editable: false,
-                                  size:
-                                      SizeConfig.instance.safeBlockHorizontal *
-                                          30,
-                                ),
-                                Expanded(
-                                  child: Container(
-                                    alignment: Alignment.topLeft,
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: SizeConfig
-                                                .instance.safeBlockHorizontal *
-                                            6),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Padding(
-                                          padding: EdgeInsets.only(
-                                            bottom: SizeConfig.instance
-                                                    .safeBlockVertical *
-                                                0.5,
-                                          ),
-                                          child: Text(
-                                            widget.user.displayName,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline6
-                                                .apply(fontWeightDelta: 2),
-                                          ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: SizeConfig.instance.safeBlockHorizontal * 6,
+                      right: SizeConfig.instance.safeBlockHorizontal * 6,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: SizeConfig.instance.safeBlockHorizontal * 30,
+                          padding: EdgeInsets.only(
+                            bottom: SizeConfig.instance.safeBlockVertical,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              ProfilePicture(
+                                photoUrl: widget.user.photoUrl,
+                                shape: BoxShape.circle,
+                                editable: false,
+                                size: SizeConfig.instance.safeBlockHorizontal *
+                                    30,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  alignment: Alignment.topLeft,
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: SizeConfig
+                                              .instance.safeBlockHorizontal *
+                                          6),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                          bottom: SizeConfig
+                                                  .instance.safeBlockVertical *
+                                              0.5,
                                         ),
-                                        Text(
-                                          widget.user.title ?? '',
+                                        child: Text(
+                                          widget.user.displayName,
                                           style: Theme.of(context)
                                               .textTheme
-                                              .bodyText1,
+                                              .headline6
+                                              .apply(fontWeightDelta: 2),
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                      Text(
+                                        widget.user.title ?? '',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1,
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                              vertical: SizeConfig.instance.safeBlockVertical,
-                            ),
-                            child: Text(widget.user.about ?? ''),
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: SizeConfig.instance.safeBlockVertical,
                           ),
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Wrap(
-                                children: widget.user.interests
-                                        ?.map((x) => Padding(
-                                              padding: EdgeInsets.only(
-                                                  right: SizeConfig.instance
-                                                          .blockSizeHorizontal *
-                                                      3),
-                                              child: InterestItem(text: x),
-                                            ))
-                                        ?.toList() ??
-                                    []),
-                          ),
-                        ],
-                      ),
+                          child: Text(widget.user.about ?? ''),
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Wrap(
+                              children: widget.user.interests
+                                      ?.map((x) => Padding(
+                                            padding: EdgeInsets.only(
+                                                right: SizeConfig.instance
+                                                        .blockSizeHorizontal *
+                                                    3),
+                                            child: InterestItem(text: x),
+                                          ))
+                                      ?.toList() ??
+                                  []),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                bottom: TabBar(
-                  controller: _tabController,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  // These are the widgets to put in each tab in the tab bar.
-                  tabs: tabs
-                      .map((String name) => Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: kTabBarTextPadding,
-                            ),
-                            child: Tab(text: name),
-                          ))
-                      .toList(),
+                ],
+              ),
+            ),
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              sliver: SliverPersistentHeader(
+                pinned: true,
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    controller: _tabController,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    // These are the widgets to put in each tab in the tab bar.
+                    tabs: tabs
+                        .map((String name) => Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: kTabBarTextPadding,
+                              ),
+                              child: Tab(text: name),
+                            ))
+                        .toList(),
+                  ),
                 ),
               ),
             ),
-
-            // Padding(
-            //   padding: EdgeInsets.only(
-            //     left: SizeConfig.instance.safeBlockHorizontal * 6,
-            //     right: SizeConfig.instance.safeBlockHorizontal * 6,
-            //   ),
-            //   child: Column(
-            //     children: <Widget>[
-            //       Container(
-            //         height: SizeConfig.instance.safeBlockHorizontal * 30,
-            //         padding: EdgeInsets.only(
-            //           bottom: SizeConfig.instance.safeBlockVertical,
-            //         ),
-            //         child: Row(
-            //           mainAxisAlignment: MainAxisAlignment.start,
-            //           children: <Widget>[
-            //             ProfilePicture(
-            //               photoUrl: widget.user.photoUrl,
-            //               shape: BoxShape.circle,
-            //               editable: false,
-            //               size: SizeConfig.instance.safeBlockHorizontal * 30,
-            //             ),
-            //             Expanded(
-            //               child: Container(
-            //                 alignment: Alignment.topLeft,
-            //                 padding: EdgeInsets.symmetric(
-            //                     horizontal:
-            //                         SizeConfig.instance.safeBlockHorizontal *
-            //                             6),
-            //                 child: Column(
-            //                   crossAxisAlignment: CrossAxisAlignment.start,
-            //                   children: <Widget>[
-            //                     Padding(
-            //                       padding: EdgeInsets.only(
-            //                         bottom:
-            //                             SizeConfig.instance.safeBlockVertical *
-            //                                 0.5,
-            //                       ),
-            //                       child: Text(
-            //                         widget.user.displayName,
-            //                         style: Theme.of(context)
-            //                             .textTheme
-            //                             .headline6
-            //                             .apply(fontWeightDelta: 2),
-            //                       ),
-            //                     ),
-            //                     Text(
-            //                       widget.user.title ?? '',
-            //                       style: Theme.of(context).textTheme.bodyText1,
-            //                     ),
-            //                   ],
-            //                 ),
-            //               ),
-            //             ),
-            //           ],
-            //         ),
-            //       ),
-            //       Container(
-            //         padding: EdgeInsets.symmetric(
-            //           vertical: SizeConfig.instance.safeBlockVertical,
-            //         ),
-            //         child: Text(widget.user.about ?? ''),
-            //       ),
-            //       Align(
-            //         alignment: Alignment.centerLeft,
-            //         child: Wrap(
-            //             children: widget.user.interests
-            //                     ?.map((x) => Padding(
-            //                           padding: EdgeInsets.only(
-            //                               right: SizeConfig.instance
-            //                                       .blockSizeHorizontal *
-            //                                   3),
-            //                           child: InterestItem(text: x),
-            //                         ))
-            //                     ?.toList() ??
-            //                 []),
-            //       ),
-            //     ],
-            //   ),
-            // ),
           ];
         },
         body: TabBarView(
@@ -336,5 +239,30 @@ class _ViewUserProfileScreenState extends State<ViewUserProfileScreen>
         ),
       ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      color: Colors.white,
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
