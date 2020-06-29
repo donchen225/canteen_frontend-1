b3966bb13e99316924a16843c80d12beffedbd95
brianbhsu <brianhsu93@gmail.com>
2020-06-29 12:29:10 -0400

    firebase setup script WIP

--- firebase-setup.sh
+++ firebase-setup.sh
@@ -5,24 +5,35 @@ ENVIRONMENT="development"
 echo "Setting up Firebase $ENVIRONMENT environment..."
 
 # Switch to firebase project to set up
+echo "Switching to Firebase $ENVIRONMENT project"
 firebase use $ENVIRONMENT
 
 # Set up Cloud Functions secrets
 ## Load algolia config
-gsutil cp gs://canteen-secrets-${ENVIRONMENT}/algolia-keys.json .
+ALGOLIA_CONFIG_FILE="gs://canteen-secrets-${ENVIRONMENT}/algolia-keys.json"
+
+echo "Copying config file $ALGOLIA_CONFIG_FILE..."
+gsutil cp $ALGOLIA_CONFIG_FILE .
 
 ALGOLIA_APP_ID=$(cat algolia-keys.json | jq -r ".ALGOLIA_APP_ID")
 ALGOLIA_ADMIN_API_KEY=$(cat algolia-keys.json | jq -r ".ALGOLIA_ADMIN_API_KEY")
 SEARCH_ONLY_API_KEY=$(cat algolia-keys.json | jq -r ".SEARCH_ONLY_API_KEY")
 
+echo "Setting Cloud Functions environment variables..."
 firebase functions:config:set algolia.appid="$ALGOLIA_APP_ID" algolia.apikey="$ALGOLIA_ADMIN_API_KEY" algolia.searchonlyapikey="$SEARCH_ONLY_API_KEY"
 
-# Generate algolia api keys
+echo "Deploying all Firebase resources..."
+firebase deploy
 
-# curl https://us-central1-get-canteen.cloudfunctions.net/setAlgoliaSearchAttributes
+echo "Generating Algolia API keys..."
+gcloud functions call generateAlgoliaSearchApiKeys
 
-# # Set up firestore entries
-# ## Create groups in firestore
+echo "Setting algolia search attributes..."
+gcloud functions call setAlgoliaSearchAttributes
+
+# Set up firestore entries
+## Create groups in firestore
+echo "Creating initial groups..."
 # for file in groups/*
 # do
 #     if [[ -f $file ]]; then
