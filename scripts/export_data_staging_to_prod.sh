187360f0ca671971a79de43083f933a8373c94e2
brianbhsu <brianhsu93@gmail.com>
2020-08-17 14:04:37 -0400

    update firebase data transfer script

--- scripts/export_data_staging_to_prod.sh
+++ scripts/export_data_staging_to_prod.sh
@@ -17,14 +17,14 @@ firebase auth:import firebase_auth_accounts.json    \
     --mem-cost=14
 
 # Export Firestore data
-# Collections: discover, groups, matches, notifications, queries, recommendations, requests, users
+# Collections: discover, groups, matches, notifications, recommendations, requests, users
 gcloud config set project get-canteen-staging
-gcloud firestore export gs://canteen-data-dump-staging --collection-ids=discover,groups,matches,notifications,recommendations,requests,users --async
+gcloud firestore export gs://canteen-data-dump-staging --collection-ids=discover,groups,members,security,posts,matches,messages,notifications,child_notifications,recommendations,requests,users,settings,tokens --async
 
-gsutil cp -r gs://canteen-data-dump-staging/ gs://canteen-data-dump-prod
+gsutil -m rsync -r gs://canteen-data-dump-staging gs://canteen-data-dump-prod
 
 gcloud config set project get-canteen-prod
-gcloud firestore import gs://canteen-data-dump-prod/canteen-data-dump-staging/2020-08-14T23:16:17_25977 --collection-ids=discover,groups,matches,notifications,recommendations,requests,users
+gcloud firestore import gs://canteen-data-dump-prod/2020-08-17T17:16:43_20377
 
 # Copy group images and user images
 gsutil -m rsync -r gs://get-canteen-staging.appspot.com gs://get-canteen-prod.appspot.com
